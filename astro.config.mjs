877baa5feaa2817c90a8a4df8009e91b78463f35
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2025-06-07 15:15:56 -0300

    initial implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Nunito",
+        cssVariable: "--font-nunito",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Fragment Mono",
+        cssVariable: "--font-fragment",
+      },
+    ],
+  },
+});
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface TeamMember {
+  name: string;
+  role: string;
+  imageUrl: ImageMetadata;
+  linkedinUrl: string;
+}
