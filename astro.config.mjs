8f81bf286ac7433c9474be2de4e2d1a16b58492f
Denilson Ebling <d.ebling8@gmail.com>
2025-06-15 18:06:42 -0300

    fix: changed to be a root link now

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zeit-ia.github.io',
-  base: 'landing-page',
 
   vite: {
     plugins: [tailwindcss()],
