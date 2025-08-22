3a7263b1fb0c64c220f0e4907cac16d601c5097f
Denilson dos Santos Ebling <d.ebling8@gmail.com>
2025-08-22 08:26:54 -0300

    prettier formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zeit.com.br',
+  site: "https://zeit.com.br",
 
   vite: {
     plugins: [tailwindcss()],
